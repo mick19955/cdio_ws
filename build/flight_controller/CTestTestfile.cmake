11caa5b6a636f4272eb7039262f05bdeb68c7cd0
Niicoliy <staehr.k@gmail.com>
2018-06-11 12:24:23 +0200

    Removed build and devel

--- build/flight_controller/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cdio/cdio_ws/src/flight_controller
-# Build directory: /home/cdio/cdio_ws/build/flight_controller
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/rosBridge_driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cdio/cdio_ws/src/rosBridge_driver
-# Build directory: /home/cdio/cdio_ws/build/rosBridge_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
